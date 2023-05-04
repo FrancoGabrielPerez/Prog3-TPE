c9c46e270e70ceb0d1fdf3816c7e1211eef27bf7
Franco Perez <FrancoGabrielPerez@gmail.com>
2023-05-04 01:14:21 -0300

    Implemented Graph

--- src/Grafo/GrafoDirigido.java
+++ src/Grafo/GrafoDirigido.java
@@ -1,85 +1,117 @@
 package Grafo;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class GrafoDirigido<T> implements Grafo<T> {
+	private HashMap<Integer, HashMap<Integer, Arco<T>>> graph;
+	private int cantArcs;
+
+	public GrafoDirigido() {
+		this.graph = new HashMap<>();
+		this.cantArcs = 0;
+	}
 
 	@Override
 	public void agregarVertice(int verticeId) {
-		// TODO Auto-generated method stub
-
+		graph.putIfAbsent(verticeId, new HashMap<>());
 	}
 
 	@Override
 	public void borrarVertice(int verticeId) {
-		// TODO Auto-generated method stub
-
+		if (! graph.containsKey(verticeId)) {
+			return;
+		}
+		cantArcs -= graph.get(verticeId).size();
+		graph.remove(verticeId);
+		graph.forEach((k,v) -> {if (v.remove(verticeId) != null){cantArcs--;}});
 	}
 
 	@Override
 	public void agregarArco(int verticeId1, int verticeId2, T etiqueta) {
-		// TODO Auto-generated method stub
+		if (graph.containsKey(verticeId1)) {
+			graph.get(verticeId1).put(verticeId2, new Arco<>(verticeId1, verticeId2, etiqueta));
+			cantArcs++;
+		}
+	}
 
+	public void agregarArco(int verticeId1, int verticeId2) {
+		agregarArco(verticeId1, verticeId2, null);
 	}
 
 	@Override
 	public void borrarArco(int verticeId1, int verticeId2) {
-		// TODO Auto-generated method stub
-
+		if (graph.containsKey(verticeId1)) {
+			if (graph.get(verticeId1).remove(verticeId2) != null) {
+				cantArcs--;
+			}
+		}
 	}
 
 	@Override
 	public boolean contieneVertice(int verticeId) {
-		// TODO Auto-generated method stub
-		return false;
+		return graph.containsKey(verticeId);
 	}
 
 	@Override
 	public boolean existeArco(int verticeId1, int verticeId2) {
-		// TODO Auto-generated method stub
-		return false;
+		return graph.containsKey(verticeId1) ? graph.get(verticeId1).containsKey(verticeId2) : false;
 	}
 
 	@Override
 	public Arco<T> obtenerArco(int verticeId1, int verticeId2) {
-		// TODO Auto-generated method stub
-		return null;
+		return graph.containsKey(verticeId1) ? graph.get(verticeId1).get(verticeId2) : null;
 	}
 
 	@Override
 	public int cantidadVertices() {
-		// TODO Auto-generated method stub
-		return 0;
+		return graph.size();
 	}
 
 	@Override
 	public int cantidadArcos() {
-		// TODO Auto-generated method stub
-		return 0;
+		return cantArcs;
 	}
 
 	@Override
 	public Iterator<Integer> obtenerVertices() {
-		// TODO Auto-generated method stub
-		return null;
+		return graph.keySet().iterator();
 	}
 
 	@Override
 	public Iterator<Integer> obtenerAdyacentes(int verticeId) {
-		// TODO Auto-generated method stub
-		return null;
+		return graph.containsKey(verticeId) ? graph.get(verticeId).keySet().iterator() : Collections.emptyIterator();
 	}
 
 	@Override
 	public Iterator<Arco<T>> obtenerArcos() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Iterator<Arco<T>>() {
+			private Iterator<HashMap<Integer, Arco<T>>> outerIterator = graph.values().iterator();
+			private Iterator<Arco<T>> innerIterator = Collections.emptyIterator();
+	
+			@Override
+			public boolean hasNext() {
+				while (!innerIterator.hasNext() && outerIterator.hasNext()) {
+					innerIterator = outerIterator.next().values().iterator();
+				}
+				return innerIterator.hasNext();
+			}
+	
+			@Override
+			public Arco<T> next() {
+				if (!hasNext()) {
+					throw new NoSuchElementException();
+				}
+				return innerIterator.next();
+			}
+		};
 	}
 
 	@Override
 	public Iterator<Arco<T>> obtenerArcos(int verticeId) {
-		// TODO Auto-generated method stub
-		return null;
+		return graph.containsKey(verticeId) ? graph.get(verticeId).values().iterator() : Collections.emptyIterator();
 	}
 
 }
--- src/TPE.java
+++ src/TPE.java
@@ -1,5 +1,43 @@
+import java.util.Iterator;
+
+import Grafo.*;
+
 public class TPE {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+		GrafoDirigido<Integer> grafo = new GrafoDirigido<>();
+		grafo.agregarVertice(1);
+		grafo.agregarVertice(2);
+		grafo.agregarVertice(3);
+		grafo.agregarVertice(4);
+		grafo.agregarVertice(5);
+		grafo.agregarVertice(10);
+		grafo.agregarVertice(20);
+		grafo.agregarVertice(7);
+		grafo.agregarVertice(13);
+		grafo.agregarArco(1, 2);
+		grafo.agregarArco(1, 3);
+		grafo.agregarArco(1, 4);
+		grafo.agregarArco(1, 5);
+		grafo.agregarArco(5, 7);
+		grafo.agregarArco(7, 13);
+		grafo.agregarArco(7, 1);
+		grafo.agregarArco(2, 4);
+		grafo.agregarArco(2, 20);
+		grafo.agregarArco(10, 10);
+		grafo.agregarArco(20, 7);
+		grafo.agregarArco(13, 2);
+		System.out.println("Arcos: " + grafo.cantidadArcos());
+		System.out.println("Vertices: " + grafo.cantidadVertices());
+		System.out.println("Vertice 1?: " + grafo.contieneVertice(1));
+		System.out.println("Vertice 6?: " + grafo.contieneVertice(6));
+		System.out.println("Arco 13-2?: " + grafo.existeArco(13, 2));
+		grafo.borrarArco(13, 2);
+		System.out.println("Arco 13-2?: " + grafo.existeArco(13, 2));
+		System.out.print("Arcos: [");
+		for (Iterator<Arco<Integer>> it = grafo.obtenerArcos(); it.hasNext();) {
+			Arco<Integer> a = it.next();
+			System.out.print(" " + a.getVerticeOrigen() + "-" + a.getVerticeDestino() + " ");
+		}
+		System.out.println("]");
+	}
 }
