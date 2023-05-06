d661927d70597258637fe99e5074611d17d13c03
Franco Perez <FrancoGabrielPerez@gmail.com>
2023-05-06 19:45:43 -0300

    implement caminos()
    Co-authored-by: MarchioriRH <MarchioriRH@users.noreply.github.com>

--- src/Servicios/ServicioCaminos.java
+++ src/Servicios/ServicioCaminos.java
@@ -1,6 +1,9 @@
 package Servicios;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 import Grafo.Grafo;
@@ -11,6 +14,8 @@ public class ServicioCaminos {
 	private int origen;
 	private int destino;
 	private int lim;
+	private int time;
+	private HashMap<Integer, HashSet<Integer>> arcos;
 	
 	// Servicio caminos
 	public ServicioCaminos(Grafo<?> grafo, int origen, int destino, int lim) {
@@ -18,11 +23,43 @@ public class ServicioCaminos {
 		this.origen = origen;
 		this.destino = destino;
 		this.lim = lim;
+		this.arcos = new HashMap<>();
 	}
 
 	public List<List<Integer>> caminos() {
-		// Resolver Caminos
-		return new ArrayList<>();
+		/* time = 1;
+		this.vertices = new HashMap<>();
+		for (Iterator<Integer> it = grafo.obtenerVertices(); it.hasNext();) {//mover a constructor
+			vertices.put(it.next(), new VertexInfo());
+		} */
+		List<List<Integer>> roads = new ArrayList<>();
+		List<Integer> aux = new ArrayList<>();
+		caminosDFS(origen, roads, aux);
+		return roads;
 	}
 
+
+	private void caminosDFS(Integer v, List<List<Integer>> roads, List<Integer> caminoActual){
+		caminoActual.add(v);
+		arcos.putIfAbsent(v, new HashSet<>());
+		if (v == destino) {
+			System.out.println(caminoActual);
+			roads.add(caminoActual);
+		}
+		Iterator<Integer> itAdjacents  = grafo.obtenerAdyacentes(v);
+		if (caminoActual.size() < lim) {			//¿< o <=?
+			while (itAdjacents.hasNext()) {
+				Integer Adjacent = itAdjacents.next();
+				if (!arcos.get(v).contains(Adjacent)) {
+					arcos.get(v).add(Adjacent);
+					caminosDFS(Adjacent, roads, caminoActual);
+					caminoActual.remove(caminoActual.size()-1);
+				} else {
+					//cyclical = true;
+				}
+			}
+			arcos.remove(v);
+		}
+		return;
+	}
 }
--- src/TPE.java
+++ src/TPE.java
@@ -1,6 +1,11 @@
 
+import java.security.Provider.Service;
+import java.util.ArrayList;
+import java.util.List;
+
 import Grafo.*;
 import Servicios.ServicioBFS;
+import Servicios.ServicioCaminos;
 import Servicios.ServicioDFS;
 
 public class TPE {
@@ -11,35 +16,39 @@ public class TPE {
 		grafo.agregarVertice(3);
 		grafo.agregarVertice(4);
 		grafo.agregarVertice(5);
-		grafo.agregarVertice(10);
-		grafo.agregarVertice(20);
+		grafo.agregarVertice(6);
 		grafo.agregarVertice(7);
 		grafo.agregarVertice(13);
 		grafo.agregarArco(1, 2);
 		grafo.agregarArco(1, 3);
 		grafo.agregarArco(1, 4);
-		grafo.agregarArco(1, 5);
-		grafo.agregarArco(5, 7);
-		grafo.agregarArco(7, 13);
-		grafo.agregarArco(7, 1);
 		grafo.agregarArco(2, 4);
-		grafo.agregarArco(2, 20);
-		grafo.agregarArco(10, 10);
-		grafo.agregarArco(20, 7);
-		grafo.agregarArco(13, 2);
-		System.out.println("Arcos: " + grafo.cantidadArcos());
-		System.out.println("Vertices: " + grafo.cantidadVertices());
-		System.out.println("Vertice 1?: " + grafo.contieneVertice(1));
-		System.out.println("Vertice 6?: " + grafo.contieneVertice(6));
-		System.out.println("Arco 13-2?: " + grafo.existeArco(13, 2));
-		grafo.borrarArco(13, 2);
-		System.out.println("Arco 13-2?: " + grafo.existeArco(13, 2));
-		System.out.println(grafo);
-		ServicioDFS dfs = new ServicioDFS(grafo);
-		System.out.println(dfs.dfsForest());
-		System.out.println(dfs);
-		ServicioBFS bfs = new ServicioBFS(grafo);
-		System.out.println(bfs.bfsForest());
-		System.out.println(bfs);
+		grafo.agregarArco(3, 4);
+		grafo.agregarArco(3, 5);
+		grafo.agregarArco(4, 6);
+		grafo.agregarArco(4, 7);
+		grafo.agregarArco(6, 2);
+		grafo.agregarArco(6, 7);
+		grafo.agregarArco(5, 1);
+		grafo.agregarArco(7, 2);
+		// System.out.println("Arcos: " + grafo.cantidadArcos());
+		// System.out.println("Vertices: " + grafo.cantidadVertices());
+		// System.out.println("Vertice 1?: " + grafo.contieneVertice(1));
+		// System.out.println("Vertice 6?: " + grafo.contieneVertice(6));
+		// System.out.println("Arco 13-2?: " + grafo.existeArco(13, 2));
+		// grafo.borrarArco(13, 2);
+		// System.out.println("Arco 13-2?: " + grafo.existeArco(13, 2));
+		// System.out.println(grafo);
+		// ServicioDFS dfs = new ServicioDFS(grafo);
+		// System.out.println(dfs.dfsForest());
+		// System.out.println(dfs);
+		// ServicioBFS bfs = new ServicioBFS(grafo);
+		// System.out.println(bfs.bfsForest());
+		// System.out.println(bfs);
+		ServicioCaminos s = new ServicioCaminos(grafo, 1, 7, 10);
+		List<List<Integer>> caminos = s.caminos();
+		for (List<Integer> camino : caminos) {
+			//System.out.println(camino);
+		}
 	}
 }
